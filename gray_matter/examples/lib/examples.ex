1f0c1be9ef0f93816fb17c00e4336c044297d44f
keiSunagawa <kei.sunagawa@mvrck.co.jp>
2019-06-10 19:24:08 +0900

    add examples

---
+++ gray_matter/examples/lib/examples.ex
@@ -0,0 +1,66 @@
+defmodule KVS do
+  import GrayMatter.Command
+  defcommand Put do
+    key :: String.t()
+    value :: non_neg_integer()
+    next :: any()
+  end
+  defcommand Get do
+    key :: String.t()
+    next :: (non_neg_integer() -> any())
+  end
+  defcommand SafeModify do
+    key :: String.t()
+    value :: non_neg_integer()
+    next ::  (boolean() -> any())
+  end
+end
+
+defmodule KVS.Inmemory do
+  alias Algae.Free.{Roll, Pure}
+  alias Algae.State
+  import Witchcraft.Chain
+  import Quark.Compose
+
+  def interpreter(%Roll{roll: %Coyoneda{f: f, m: m}}) do
+    case m do
+      %KVS.Put{key: k, value: v, next: n} ->
+        State.state(fn st ->  {n, Map.put(st, k, v)} end)
+        >>> ((&interpreter/1) <|> f)
+      %KVS.Get{key: k, next: nf} ->
+        State.state(fn st ->
+          {_, v} = Map.fetch(st, k)
+          {nf.(v), st}
+        end) >>> ((&interpreter/1) <|> f)
+      %KVS.SafeModify{key: k, value: v, next: nf} ->
+        State.state(fn st ->
+          case  Map.fetch(st, k) do
+            {:ok, _} -> {nf.(false), st}
+            _ -> {nf.(true), Map.put(st, k, v)}
+          end
+        end) >>> ((&interpreter/1) <|> f)
+    end
+  end
+  def interpreter(%Pure{pure: a}) do
+    IO.inspect(a)
+    State.state(fn st ->  {a, st} end)
+  end
+end
+
+defmodule Examples do
+  import KVS
+  import Witchcraft.Chain
+  def run do
+    m = chain do
+      put("a", 1)
+      o <- get("a")
+      a <- safemodify("a", 2)
+      let _ = log([o, a])
+      get("a")
+    end
+
+    KVS.Inmemory.interpreter(m).runner.(%{})
+  end
+
+  def log(xs), do: IO.inspect(xs)
+end
--- gray_matter/lib/gray_matter.ex
+++ gray_matter/lib/gray_matter.ex
@@ -1,22 +1,2 @@
 defmodule GrayMatter do
-  import GrayMatter.Command
-
-  defcommand Put do
-    a :: String.t()
-    next :: any()
-  end
-  defcommand Get do
-    next :: ({non_neg_integer(), String.t()} -> any())
-  end
-  defcommand Update do
-    arg :: String.t()
-    next ::  (String.t() -> any())
-  end
-end
-
-defmodule BB do
-  def run do
-    GrayMatter.Put.new("aa", nil)
-    GrayMatter.put("a")
-  end
 end
